e3ec5aea3dcaf767867b33dcb236371dc190fe87
viru0909-dev <gadekarvirendra4@gmail.com>
2025-10-19 22:16:51 +0530

    Binary Number to decimal Number

--- MultiThreading/src/main/java/com/java_practice/problems/binary.java
+++ MultiThreading/src/main/java/com/java_practice/problems/binary.java
@@ -1,18 +1,34 @@
 package com.java_practice.problems;
 
 public class binary {
- public static int decToBinary(int n){
-     int sum = 0, pow = 1;
-     while (n > 0){
+
+    //converting a decimal Number to Binary Number
+    public static int decToBinary(int n){
+        int sum = 0, pow = 1;
+        while (n > 0){
          int rem = n % 2;
          n /= 2;
          sum += (pow * rem);
          pow *= 10;
-     }
-     return sum;
- }
+        }
+        return sum;
+    }
+
+
+    //converting a binary number to decimal Number
+    public static int binToDec(int n){
+        int sum = 0, pow = 1;
+        while (n > 0){
+            int rem = n % 10;
+            sum += rem * pow;
+            n /= 10;
+            pow *= 2;
+        }
+        return sum;
+    }
 
     public static void main(String[] args) {
         System.out.println(decToBinary(50));
+        System.out.println(binToDec(110010));
     }
 }
